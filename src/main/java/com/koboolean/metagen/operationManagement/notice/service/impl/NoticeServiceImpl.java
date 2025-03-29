5365343cbb7d44d51dc3932067e380d3440060cd
정의철 <giwa1011110@gmail.com>
2025-03-29 09:17:52 +0900

    MET-12 : notice 부분 1차 커밋

--- src/main/java/com/koboolean/metagen/operationManagement/notice/service/impl/NoticeServiceImpl.java
+++ src/main/java/com/koboolean/metagen/operationManagement/notice/service/impl/NoticeServiceImpl.java
@@ -1,35 +1,47 @@
 package com.koboolean.metagen.operationManagement.notice.service.impl;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import org.hibernate.query.Page;
-import org.springdoc.core.converters.models.Pageable;
-import org.springframework.data.domain.Sort;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import com.koboolean.metagen.board.domain.dto.BoardDto;
 import com.koboolean.metagen.board.domain.entity.Board;
+import com.koboolean.metagen.grid.domain.dto.ColumnDto;
+import com.koboolean.metagen.grid.enums.ColumnType;
 import com.koboolean.metagen.operationManagement.notice.repository.NoticeRepository;
 import com.koboolean.metagen.operationManagement.notice.service.NoticeService;
+import com.koboolean.metagen.security.domain.dto.AccountDto;
 
 import lombok.RequiredArgsConstructor;
 
 @Service
 @RequiredArgsConstructor
-@Transactional(readOnly = true)
 public class NoticeServiceImpl implements NoticeService {
 
     private final NoticeRepository noticeRepository;
 
-    /**
-     *  공지사항 리스트 조회
+    /** [공지사항 컬럼 조회]
+     *  공지사항 그리드에 표시할 컬럼을 조회한다.
+     *  @return
      */
-    public List<BoardDto> selectNoticeList() {
+    @Override
+    public List<ColumnDto> getNoticeListColumn() {
 
-        List<BoardDto> selectNoticeList = noticeRepository.findBy[ProjectId]();
+        return List.of(new ColumnDto("게시글 번호", "id"        , ColumnType.NUMBER)
+                     , new ColumnDto("게시글 제목", "title"     , ColumnType.STRING)
+                    );
+    }
 
-        return selectNoticeList;
+    /** [공지사항 리스트 조회]
+     *  공지사항 그리드에 표시할 데이터를 조회한다.
+     */
+    @Override
+    public Page<BoardDto> getNoticeList(Pageable pageable, AccountDto accountDto) {
+
+        Page<Board> allByProjectId = noticeRepository.findAllByProjectId(accountDto.getProjectId(), pageable);
+
+        return allByProjectId.map(BoardDto::fromEntity);
     }
 }
