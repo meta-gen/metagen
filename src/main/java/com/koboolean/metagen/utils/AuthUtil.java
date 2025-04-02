51bbddc7e89bc6563f47a058e1e11fcba05c893e
조현준 <45157196+koboolean@users.noreply.github.com>
2025-04-02 19:55:07 +0900

    MET-15 프로젝트 로그인 한 계정이 Project 관리자 계정일 경우 isIsApprovalAvailable가 true가 될 수 있도록 변경

--- src/main/java/com/koboolean/metagen/utils/AuthUtil.java
+++ src/main/java/com/koboolean/metagen/utils/AuthUtil.java
@@ -1,20 +1,40 @@
 package com.koboolean.metagen.utils;
 
+import com.koboolean.metagen.security.domain.dto.AccountDto;
+import com.koboolean.metagen.system.project.service.ProjectManageService;
+import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
 
+@Component
+@RequiredArgsConstructor
 public class AuthUtil {
 
     private static final Logger log = LoggerFactory.getLogger(AuthUtil.class);
+    private static ProjectManageService projectManageService;
+
+    @Autowired
+    public AuthUtil(ProjectManageService projectManageService) {
+        AuthUtil.projectManageService = projectManageService;
+    }
 
     public static boolean isIsApprovalAvailable() {
         // 디버깅 로그 추가
-        return SecurityContextHolder.getContext().getAuthentication().getAuthorities()
+        AccountDto accountDto = (AccountDto) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        boolean isLoginManageProject = projectManageService.selectLoginProject(accountDto.getProjectId(), accountDto.getId());
+
+        boolean isAuthorities = SecurityContextHolder.getContext().getAuthentication().getAuthorities()
                 .stream()
                 .map(GrantedAuthority::getAuthority)
                 .peek(log::debug) // 디버깅 로그 추가
                 .anyMatch(role -> role.equals("ROLE_ADMIN") || role.equals("ROLE_MANAGER"));
+
+        return isLoginManageProject || isAuthorities;
     }
+
+
 }
