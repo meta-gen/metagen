8c81ede4a091f56d04585377eaf56aa6103a62b2
조현준 <45157196+koboolean@users.noreply.github.com>
2025-05-05 14:24:11 +0900

    설계서 및 테스트 시나리오 스크립트 부 및 인가 관련사항 반영

--- src/main/resources/data-h2.sql
+++ src/main/resources/data-h2.sql
@@ -163,6 +163,8 @@ merge into RESOURCES (resource_id, http_method, order_num, resource_name, resour
 merge into RESOURCES (resource_id, http_method, order_num, resource_name, resource_type, resource_desc, created_by, updated_by) key(resource_id) values (61, '*', 0, '/popup/noticePopupDetail','url', '공지사항 상세보기 팝업', 'SYSTEM', 'SYSTEM');
 merge into RESOURCES (resource_id, http_method, order_num, resource_name, resource_type, resource_desc, created_by, updated_by) key(resource_id) values (62, '*', 0, '/popup/noticePopupSave','url', '공지사항 등록 팝업', 'SYSTEM', 'SYSTEM');
 merge into RESOURCES (resource_id, http_method, order_num, resource_name, resource_type, resource_desc, created_by, updated_by) key(resource_id) values (63, '*', 0, '/','url', '대시보드', 'SYSTEM', 'SYSTEM');
+merge into RESOURCES (resource_id, http_method, order_num, resource_name, resource_type, resource_desc, created_by, updated_by) key(resource_id) values (64, '*', 0, '/api/printDesign/**','url', '설계서 출력', 'SYSTEM', 'SYSTEM');
+merge into RESOURCES (resource_id, http_method, order_num, resource_name, resource_type, resource_desc, created_by, updated_by) key(resource_id) values (64, '*', 0, '/api/printTest/**','url', '시나리오 출력', 'SYSTEM', 'SYSTEM');
 
 -- ROLE_NOT_APPROVE
 merge into role_resources(resource_id, role_id) key(resource_id, role_id) values (1,5); -- 로그아웃
@@ -214,6 +216,8 @@ merge into role_resources(resource_id, role_id) key(resource_id, role_id) values
 merge into role_resources(resource_id, role_id) key(resource_id, role_id) values (57,3); -- 코드규칙 저장
 merge into role_resources(resource_id, role_id) key(resource_id, role_id) values (58,3); -- 코드규칙 삭제
 merge into role_resources(resource_id, role_id) key(resource_id, role_id) values (59,3); -- 코드규칙 팝업
+merge into role_resources(resource_id, role_id) key(resource_id, role_id) values (64,3); -- 설계서 출력
+merge into role_resources(resource_id, role_id) key(resource_id, role_id) values (65,3); -- 시나리오 출력
 
 -- ROLE_DBA
 merge into role_resources(resource_id, role_id) key(resource_id, role_id) values (31,2) ; -- 프로젝트저장
